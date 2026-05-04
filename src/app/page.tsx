e803ed4ce5758ea3f0414a9404c8d0cc63ffbf5e
staraircool <295starboy@gmail.com>
2026-05-04 23:05:40 +0500

    chore: clean up project for amour salon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,10 @@
 import Hero from "@/components/Hero";
-import Manifesto from "@/components/Manifesto";
-import Reels from "@/components/Reels";
-import Lookbook from "@/components/Lookbook";
-import Gallery from "@/components/Gallery";
-import ServicesMenu from "@/components/ServicesMenu";
-import StylistsProfiles from "@/components/StylistsProfiles";
-import ClientTestimonials from "@/components/ClientTestimonials";
-import LiveAvailability from "@/components/LiveAvailability";
-import ClientCounter from "@/components/ClientCounter";
-import InstagramFeed from "@/components/InstagramFeed";
-import PriceCalculator from "@/components/PriceCalculator";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-black">
       <Hero />
-      <LiveAvailability />
-      <Manifesto />
-      <Reels />
-      <ServicesMenu />
-      <PriceCalculator />
-      <ClientCounter />
-      <StylistsProfiles />
-      <Lookbook />
-      <Gallery />
-      <InstagramFeed />
-      <ClientTestimonials />
       <Footer />
     </main>
   );
